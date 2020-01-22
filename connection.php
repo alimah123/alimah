63084134145e89b6552bc3aeed1d72950b694e58
unknown <nisa@smknusabangsa.sch.id>
2020-01-21 20:20:28 -0800

    Pertama

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$connect = mysqli_connect ('localhost','root','','latihan3');
+if ($connect)
+    echo "berhasil";
+else
+    echo " maaf skoneksi gagal";
+?>
---
+++ delete.php
@@ -0,0 +1,42 @@
+<?php
+
+include ('connection.php');
+
+$query = mysqli_query($connect,"SELECT * FROM karyawan");
+$results = mysqli_fetch_all($query, MYSQLI_ASSOC);
+?>
+
+<html>
+<body> 
+    <a href="add.php">Tambah Data</a>
+
+    <br><br/>
+
+    <table border="1">
+        <tr>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Umur</th>
+            <th>Jenis Kelamin</th>
+            <th>jabatan</th>
+            <th>Pilihan</th>
+            
+            
+        </tr>
+        <?php foreach($results as $result) : ?>
+        <tr>
+            <td><?php echo $result['nama']?></td>
+            <td><?php echo $result['alamat']?></td>
+            <td><?php echo $result['umur']?></td>
+            <td><?php echo $result['jenis_kelamin']?></td>
+            <td><?php echo $result['jabatan']?></td>
+            
+            <td>
+                <a href="edit.php?id=<?php echo $result['id']?>">Edit</a> 
+                <a href="delete.php?id=<?php echo $result['id']?>">delete</a>
+            </td>
+        </tr>
+<?php endforeach; ?>
+    </table>
+</body>
+</html>
